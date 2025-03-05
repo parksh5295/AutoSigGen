828aecb4c56c5f2da451962883370f4643e7f1f0
parksh5295 <parksh5295@gmail.com>
2025-03-05 09:29:20 +0900

    first commit

---
+++ Clustering_Method/clustering_score.py
@@ -0,0 +1,29 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, jaccard_score, silhouette_score
+import numpy as np
+
+
+def evaluate_clustering(y_true, y_pred, X_data):
+    if not y_true.empty:
+        return {
+            "macro_accuracy": accuracy_score(y_true, y_pred),
+            "macro_precision": precision_score(y_true, y_pred, average='macro', zero_division=0),
+            "macro_recall": recall_score(y_true, y_pred, average='macro', zero_division=0),
+            "macro_f1_score": f1_score(y_true, y_pred, average='macro', zero_division=0),
+            "macro_jaccard": jaccard_score(y_true, y_pred, average='macro', zero_division=0),
+            "macro_silhouette": silhouette_score(X_data, y_pred) if len(set(y_pred)) > 1 else np.nan,
+            
+            "micro_accuracy": accuracy_score(y_true, y_pred),
+            "micro_precision": precision_score(y_true, y_pred, average='micro', zero_division=0),
+            "micro_recall": recall_score(y_true, y_pred, average='micro', zero_division=0),
+            "micro_f1_score": f1_score(y_true, y_pred, average='micro', zero_division=0),
+            "micro_jaccard": jaccard_score(y_true, y_pred, average='micro', zero_division=0),
+            "micro_silhouette": silhouette_score(X_data, y_pred) if len(set(y_pred)) > 1 else np.nan,
+            
+            "weighted_accuracy": accuracy_score(y_true, y_pred),
+            "weighted_precision": precision_score(y_true, y_pred, average='weighted', zero_division=0),
+            "weighted_recall": recall_score(y_true, y_pred, average='weighted', zero_division=0),
+            "weighted_f1_score": f1_score(y_true, y_pred, average='weighted', zero_division=0),
+            "weighted_jaccard": jaccard_score(y_true, y_pred, average='weighted', zero_division=0),
+            "weighted_silhouette": silhouette_score(X_data, y_pred) if len(set(y_pred)) > 1 else np.nan,
+        }
+    return {}
