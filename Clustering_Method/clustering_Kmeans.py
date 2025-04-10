fdee732646817d2f6cc18a5cf9dd30ae35d3ae6a
parksh5295 <parksh5295@gmail.com>
2025-04-11 05:29:23 +0900

    K

---
+++ Clustering_Method/clustering_Kmeans.py
@@ -0,0 +1,42 @@
+# input 'X' is X_reduced or X rows
+# (pre)Return: Cluster Information(0, 1 Classification), num_clusters(result), Cluster Information(not fit, Non-classification, optional) overall dictionary
+# (main)Return: dictionary{Cluster Information(0, 1 Classification), best_parameter_dict}
+
+import numpy as np
+from sklearn.cluster import KMeans
+from utils.progressing_bar import progress_bar
+from Tuning_hyperparameter.Loop_elbow_gs import loop_tuning
+from Clustering_Method.clustering_nomal_identify import clustering_nomal_identify
+
+
+def clustering_Kmeans(data, X, max_clusters): # main clustering
+    clustering_result_dict = loop_tuning(data, X, 'Kmeans', max_clusters)
+    n_clusters = clustering_result_dict['optimul_cluster_n']
+    best_parameter_dict = clustering_result_dict['best_parameter_dict']
+
+    kmeans = KMeans(n_clusters=n_clusters, random_state=best_parameter_dict['random_state'], n_init=best_parameter_dict['n_init'])
+    clusters = kmeans.fit_predict(X)
+    data['cluster'] = clustering_nomal_identify(data, clusters, n_clusters)
+
+    predict_kmeans = data['cluster']
+
+    return {
+        'Cluster_labeling': predict_kmeans,
+        'Best_parameter_dict': best_parameter_dict
+    }
+
+
+def pre_clustering_Kmeans(data, X, n_clusters, random_state, n_init):
+    # Apply KMeans Clustering
+    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state, n_init=n_init)   # default; randomm_state=42, n_init=10
+
+    cluster_labels = kmeans.fit_predict(X)
+
+    predict_kmeans = clustering_nomal_identify(data, cluster_labels, n_clusters)
+    num_clusters = len(np.unique(predict_kmeans))  # Counting the number of clusters
+
+    return {
+        'Cluster_labeling' : predict_kmeans,
+        'n_clusters' : num_clusters,
+        'before_labeling' : kmeans
+    }
