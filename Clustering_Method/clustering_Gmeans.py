e4845b435f6c0dbcaea4e873ee34560c46261f71
parksh5295 <parksh5295@gmail.com>
2025-04-10 11:51:43 +0900

    .

--- Clustering_Method/clustering_Gmeans.py
+++ Clustering_Method/clustering_Gmeans.py
@@ -64,6 +64,7 @@ class GMeans:
                     clusters.append((sub_data, cluster_id))
                 else:  # Follow regularity to confirm clusters
                     self.labels_[self.labels_ == cluster_idx] = cluster_id
+                    print(f"regul cluster ID: {cluster_id}")
                 
                 cluster_id += 1
 
