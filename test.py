9af2fdccf0ccd3cf61943926ec82b3f9ecf16924
parksh5295 <parksh5295@gmail.com>
2025-04-12 19:04:28 +0900

    .

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-import pandas as pd
-
-
-chunk_size = 10
-for chunk in pd.read_csv('../Dataset/load_dataset/MiraiBotnet/output-dataset_ESSlab.csv', chunksize=chunk_size):
-    print(chunk.head(6))
+# with open('D:\AutoSigGen_withData\Dataset\load_dataset\CICIDS2017\MachineLearningCSV\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv', 'r') as f:
+with open('D:\AutoSigGen_withData\Dataset\load_dataset\NSL-KDD\NSL-KDD_dataset.csv', 'r') as f:
+    row_count = sum(1 for line in f)
+    print("count row: ", row_count)
