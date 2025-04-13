44433b44347716398e976108af0ff72f7f242f6e
parksh5295 <parksh5295@gmail.com>
2025-04-13 12:36:17 +0900

    DARPA

--- utils/time_transfer.py
+++ utils/time_transfer.py
@@ -9,21 +9,31 @@ def time_scalar_transfer(data, file_type):
         data['StartTime'] = data['StartTime'].astype(str).str.strip()
         data['Duration'] = data['Duration'].astype(str).str.strip()
 
-        # date transfer
-        try:
-            # First try with a 2-digit year
-            data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y')
-        except Exception as e1:
-            print(f"[DEBUG] First format failed. Error type: {type(e1).__name__}")
-            print(f"[DEBUG] at {data}")
-            print(f"[DEBUG] Error message: {e1}")
-            try:
-                # Then try with a 4-digit year
-                data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%y')
-            except Exception as e2:
-                print(f"[DEBUG] Second format failed. Error type: {type(e2).__name__}")
-                print(f"[DEBUG] at {data}")
-                print(f"[DEBUG] Error message: {e2}")
+        regex2 = r'^\d{2}/\d{2}/\d{2}$'
+        regex4 = r'^\d{2}/\d{2}/\d{4}$'
+        
+        mask2 = data['Date'].str.match(regex2)
+        mask4 = data['Date'].str.match(regex4)
+
+        data.loc[mask2, 'Date'] = pd.to_datetime(data.loc[mask2, 'Date'],  format="%m/%d/%y")
+        data.loc[mask4, 'Date'] = pd.to_datetime(data.loc[mask4, 'Date'],  format="%m/%d/%Y")
+        print('[DEBUG] datetime conversion complete')
+
+        # # date transfer
+        # try:
+        #     # First try with a 2-digit year
+        #     data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y')
+        # except Exception as e1:
+        #     print(f"[DEBUG] First format failed. Error type: {type(e1).__name__}")
+        #     # print(f"[DEBUG] at {data}")
+        #     print(f"[DEBUG] Error message: {e1}")
+        #     try:
+        #         # Then try with a 4-digit year
+        #         data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%y')
+        #     except Exception as e2:
+        #         print(f"[DEBUG] Second format failed. Error type: {type(e2).__name__}")
+        #         # print(f"[DEBUG] at {data}")
+        #         print(f"[DEBUG] Error message: {e2}")
 
         # Time string to seconds
         def hms_to_seconds(hms_str):
