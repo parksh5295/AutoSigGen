12b679527f570bc0376fd6c03954499c4a1baea0
parksh5295 <parksh5295@gmail.com>
2025-04-17 16:01:03 +0900

    .

--- Validate_Signature.py
+++ Validate_Signature.py
@@ -94,7 +94,7 @@ def main():
 
 
     mapped_info_path = f"../Dataset/signature/{file_type}/{file_type}_{file_number}_mapped_info.csv"
-    association_result_path = f"../Dataset/signature/{file_type}/{file_type}_{association_rule}_{file_number}_{association_metric}_signature_train.csv"
+    association_result_path = f"../Dataset/signature/{file_type}/{file_type}_{association_rule}_{file_number}_{association_metric}_signature_train_ea{signature_ea}.csv"
     
     # Load data in an optimized way
     mapped_info_df = load_csv_safely('DARPA', mapped_info_path)
